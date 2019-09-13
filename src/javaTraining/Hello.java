57ee250623c7c4e0e8f8981308dc373ee65e4034
joohae <joohae@DESKTOP-GQU34AD>
2019-09-14 03:07:59 +0900

    2557

--- src/javaTraining/Hello.java
+++ src/javaTraining/Hello.java
@@ -2,7 +2,7 @@ package javaTraining;
 //น้มุ 2557
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello Wold!");
+		System.out.println("Hello World!");
 	}
 	
 
