0328233af125a42cee64b93368768a85bb01318d
joohae <joohae@DESKTOP-GQU34AD>
2019-09-22 14:24:45 +0900

    백준 10718

--- src/javaTraining/Hello.java
+++ src/javaTraining/Hello.java
@@ -2,7 +2,8 @@ package javaTraining;
 //백준 2557
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("강한친구 대한육군");
+		System.out.println("강한친구 대한육군");
 	}
 	
 
