21bc7be7f135c57e9c00d26d113b58df99d4e0fa
joohae <joohae@DESKTOP-GQU34AD>
2019-10-06 22:47:32 +0900

    백준 2439

--- src/javaTraining/plus.java
+++ src/javaTraining/plus.java
@@ -1,5 +1,25 @@
 package javaTraining;
 
-public class plus {
+import java.util.Scanner;
 
-}
+//น้มุ 
+public class plus {
+	
+		public static void main(String args[]) {
+			Scanner s= new Scanner(System.in);
+			int a=s.nextInt();
+			
+			for(int i=0;i<a;i++) {
+				for(int j=a;j>i;j--)
+					System.out.printf(" ");
+				for(int k=0;k<=i;k++)
+					System.out.printf("*");
+				System.out.println();
+						
+				}		
+		}
+
+	}
+
+
+
