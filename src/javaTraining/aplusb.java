23d0388ece5a615197406656724de7668e228e0b
joohae <joohae@DESKTOP-GQU34AD>
2019-11-04 01:11:33 +0900

    백준 2558

---
+++ src/javaTraining/aplusb.java
@@ -0,0 +1,19 @@
+package javaTraining;
+
+import java.util.Scanner;
+
+public class aplusb {
+	
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		
+		int a=s.nextInt();
+		int b=s.nextInt();
+		
+		System.out.println(a+b);
+		
+		
+	}
+	
+
+}
