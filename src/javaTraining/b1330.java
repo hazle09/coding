aa6be985c376a0f2f1ccbe27a5c2d776851fecc8
joohae <joohae@DESKTOP-GQU34AD>
2019-11-18 00:10:02 +0900

    백준 1330

---
+++ src/javaTraining/b1330.java
@@ -0,0 +1,20 @@
+package javaTraining;
+
+import java.util.Scanner;
+
+public class b1330 {
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		
+		int a=s.nextInt();
+		int b=s.nextInt();
+		
+		if(a>b)
+			System.out.println(">");
+		if(a<b)
+			System.out.println("<");
+		if(a==b)
+			System.out.println("==");
+	}
+
+}
