8cb2d137358d96ec143533cd184581e5fb5e5dd6
joohae <joohae@DESKTOP-GQU34AD>
2019-12-22 22:28:57 +0900

    백준 2884

--- src/javaTraining/baekjoon2884.java
+++ src/javaTraining/baekjoon2884.java
@@ -18,11 +18,13 @@ public class baekjoon2884 {
 			}
 			nhour=H-1;
 			nminute=10+(60-45);
-	}
+		}
 		if(M>=45) {
-			if(H==0)
+			if(H==0) {
 				nhour=24;
-			nminute=45-M;
+			}
+			nhour=H;
+			nminute=M-45;
 		}
 		System.out.println(nhour+" "+nminute);
 		s.close();
