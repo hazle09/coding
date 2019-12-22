c91f4965e14cb574d1572b0bf0ee7f7080b971a6
joohae <joohae@DESKTOP-GQU34AD>
2019-12-22 23:55:28 +0900

    백준 10818

--- src/javaTraining/baekjoon10818.java
+++ src/javaTraining/baekjoon10818.java
@@ -4,25 +4,28 @@ import java.util.Scanner;
 
 public class baekjoon10818 {
 	
-	/*
+	
 	 public static void main(String args[]) {
-	 
 		Scanner s=new Scanner(System.in);
 		int num=s.nextInt();
-		int max=1000000;
-		int min=-1000000;
+		int max=0;
+		int min=0;
 		
-		int[]arr1=new int[num];
+		max=-1000000;
+		min=1000000;
 		
-		for(int i=0;i<arr1.length;i++) {
-			arr1[i] =s.nextInt();
-			min = Math.min(min, arr1[i]);
-			max = Math.max(max, arr1[i]);
+		int arr[]=new int[num];
+		
+		for(int i=0;i<arr.length;i++) {
+			arr[i] =s.nextInt();
+			min = Math.min(min, arr[i]);
+			max = Math.max(max, arr[i]);
 	}
 	
 		System.out.println(min+" "+max);
-		*/
+		
 	
+	/*
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		int arr[];
@@ -42,5 +45,6 @@ public class baekjoon10818 {
 		
 		System.out.print(min + " " + max);
 		
+		*/
 	}
 }
