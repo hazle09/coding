d354041b5cf36a61bc4d67ae2abe8199f9ec2194
joohae <joohae@DESKTOP-GQU34AD>
2019-12-22 22:24:55 +0900

    백준 2884

--- src/javaTraining/baekjoon2884.java
+++ src/javaTraining/baekjoon2884.java
@@ -1,5 +1,31 @@
 package javaTraining;
 
-public class baekjoon2884 {
+import java.util.Scanner;
 
-}
+public class baekjoon2884 {
+	
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		
+		int H=s.nextInt();
+		int M=s.nextInt();
+		int nminute=0;
+		int nhour = 0;
+		
+		if(M<45) {
+			if(H==0) {
+				nhour=24;
+			}
+			nhour=H-1;
+			nminute=10+(60-45);
+	}
+		if(M>=45) {
+			if(H==0)
+				nhour=24;
+			nminute=45-M;
+		}
+		System.out.println(nhour+" "+nminute);
+		s.close();
+
+	}
+	}
