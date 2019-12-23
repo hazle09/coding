5937014792ecdada294de5b49571ae98414d9891
joohae <joohae@DESKTOP-GQU34AD>
2019-12-23 23:19:07 +0900

    2920

---
+++ src/javaTraining/baekjoon2920.java
@@ -0,0 +1,18 @@
+package javaTraining;
+
+import java.util.Scanner;
+
+public class baekjoon2920 {
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		
+		
+		if(a==) 
+			System.out.println("ascending");
+		else if(a==87654321)
+			System.out.println("descending");
+		else System.out.println("mixed");
+		}
+	}
+
+
