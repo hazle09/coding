781c60c0e6fca64d8dbf7804a1971869c1fd3ca1
joohae <joohae@DESKTOP-GQU34AD>
2019-12-23 23:18:57 +0900

    2562

---
+++ src/javaTraining/baekjoon2562.java
@@ -0,0 +1,19 @@
+package javaTraining;
+
+import java.util.Scanner;
+
+public class baekjoon2562 {
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		int max=-1000000;
+		int arr[];
+		
+		while(s.hasNextInt()) {
+			int i=0;
+			arr[i]=s.nextInt();
+			
+		}
+		
+	}
+
+}
