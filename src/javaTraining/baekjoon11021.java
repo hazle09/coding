aea5ff48d489f237ddb50fb802d0c11c64d53af3
joohae <joohae@DESKTOP-GQU34AD>
2019-12-23 22:29:08 +0900

    백준 11021

---
+++ src/javaTraining/baekjoon11021.java
@@ -0,0 +1,20 @@
+package javaTraining;
+
+import java.util.Scanner;
+
+public class baekjoon11021 {
+	public static void main(String args[]) {
+		Scanner s=new Scanner(System.in);
+		
+		int num=s.nextInt();
+		int result=0;
+		
+		for(int i=1;i<=num;i++) {
+		int a=s.nextInt();
+		int b=s.nextInt();
+		result=a+b;
+		if(a>0&&b<10)
+			System.out.println("Case #"+i+": "+result);
+		}
+}
+}
