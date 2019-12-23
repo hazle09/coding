a73c990e28f66d23864b4ccf7b779cc8d94ecd25
joohae <joohae@DESKTOP-GQU34AD>
2019-12-23 22:59:06 +0900

    baekjoon 10950

---
+++ src/javaTraining/baekjoon10950.java
@@ -0,0 +1,22 @@
+package javaTraining;
+
+import java.util.Scanner;
+
+public class baekjoon10950 {
+	public static void main(String args[]) {
+
+			Scanner s=new Scanner(System.in);
+			
+			int num=s.nextInt();
+			int result=0;
+			
+			for(int i=1;i<=num;i++) {
+			int a=s.nextInt();
+			int b=s.nextInt();
+			result=a+b;
+			if(a>0&&b<10)
+				System.out.println(result);
+			}
+
+}
+}
