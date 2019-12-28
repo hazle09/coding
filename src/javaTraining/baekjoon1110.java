0c193545f24c5056ea8621ef677ffffa4879c4de
joohae <joohae@DESKTOP-GQU34AD>
2019-12-28 22:38:59 +0900

    1110

--- src/javaTraining/baekjoon1110.java
+++ src/javaTraining/baekjoon1110.java
@@ -7,19 +7,20 @@ public class baekjoon1110 {
 		Scanner s=new Scanner(System.in);
 		
 		int num=s.nextInt();
-		int m=num;
-		int count=0;
+		int after=num;
+		int count=1;
 		
 		while(true) {
 			int i=num/10; //십의자리
 			int j=num%10; //일의자리
 			int k=i+j;
 			int l=k%10;
-			num=j+l;
-			if(m==num)
+			num=j*10+l;
+			if(after==num)
 				break;
-			else
+			else {
 				count++;
+			}
 		}
 		System.out.println(count);
 		s.close();
