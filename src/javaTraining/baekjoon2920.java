2864ed403fb77d2960790d3e96558f2d36e4cfb0
joohae <joohae@DESKTOP-GQU34AD>
2019-12-28 20:01:45 +0900

    백준 2920

--- src/javaTraining/baekjoon2920.java
+++ src/javaTraining/baekjoon2920.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class baekjoon2920 {
 	public static void main(String args[]) {
 		Scanner s=new Scanner(System.in);
+		String asc="1 2 3 4 5 6 7 8";
+		String des="8 7 6 5 4 3 2 1";
 		
 		
-		if(a==) 
+		String input=s.nextLine();
+		
+		
+		if(input.contentEquals(asc)) 
 			System.out.println("ascending");
-		else if(a==87654321)
+		else if(input.contentEquals(des))
 			System.out.println("descending");
 		else System.out.println("mixed");
+		
+		s.close();
 		}
 	}
 
